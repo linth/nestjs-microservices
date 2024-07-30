ba41b85f9ca45341b90598f1a11faed710b23c83
linth <zhua0404@gmail.com>
2024-07-30 18:58:02 +0800

    add ms-app

---
+++ ms-app/src/app.controller.ts
@@ -0,0 +1,26 @@
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+import { ClientProxy } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
+
+@Controller()
+export class AppController {
+  constructor(
+    private readonly appService: AppService,
+    @Inject('MATH_SERVICE') 
+    private client: ClientProxy
+  ) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('/math/wordcount')
+  wordCount(
+    @Body() { text }: { text: string },
+  ): Observable<{ [key: string]: number }> {
+    this.client.emit('math:wordcount_log', text)
+    return this.client.send('math:wordcount', text);
+  }
+}
